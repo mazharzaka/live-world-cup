44e6931d5cf069b4eca2cb0366a560170a540d2d
mazharzaka <mazharmohamed566@gmail.com>
2026-06-15 19:26:29 +0300

    chore: update .gitignore with build directories and env patterns and refactor server debug logging

--- .next/dev/server/chunks/_next-internal_server_app_api_schedule_route_actions_02qf8b-.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/schedule/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_schedule_route_actions_02qf8b-.js.map
